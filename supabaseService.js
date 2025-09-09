f1317d7fcd41740a127cbd37f7df64e5ac5bc975
bladerangel <bladerangel10@gmail.com>
2025-09-09 15:41:02 -0300

    Fix timezone brasil

--- supabaseService.js
+++ supabaseService.js
@@ -1,18 +1,19 @@
 import { createClient } from "@supabase/supabase-js"
+import { format } from "date-fns-tz"
 
 const supabase = createClient(
     process.env.SUPABASE_URL,
     process.env.SUPABASE_KEY
 )
 
-function formatISOSecondsLocal(date) {
-    return new Date(
-        date.getTime() - date.getTimezoneOffset() * 60000
-    ).toISOString()
+function getNowBrazilISO() {
+    return format(new Date(), "yyyy-MM-dd'T'HH:mm:ss.SSS", {
+        timeZone: "America/Sao_Paulo"
+    })
 }
-
 export async function insertWebhook(origin, payload) {
-    const created = formatISOSecondsLocal(new Date())
+    const created = getNowBrazilISO()
+    console.log(created)
     const { data, error } = await supabase
         .from("webhook")
         .insert([{ origin, payload, created }])
