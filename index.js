65bba0473d5ceb0465e451a18664981b360c9d89
bladerangel <bladerangel10@gmail.com>
2025-09-09 14:11:52 -0300

    Test

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.post("/webhook", async (request, response) => {
+    console.log("host:", request.get("host"))
+    console.log("origin:", request.get("origin"))
+    console.log("referer:", request.get("referer"))
     const result = await supabaseService.insertWebhook(
         request.get("host"),
         request.body
