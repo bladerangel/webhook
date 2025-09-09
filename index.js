ae4a8ec8f8865fb82204e935507d57ab7ac06a4d
bladerangel <bladerangel10@gmail.com>
2025-09-09 14:22:34 -0300

    Add request ip

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 import "dotenv/config"
 import express from "express"
-
+import requestIp from "request-ip"
 import * as supabaseService from "./supabaseService.js"
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(requestIp.mw())
 
 app.post("/webhook", async (request, response) => {
-    console.log("host:", request.get("host"))
-    console.log("origin:", request.get("origin"))
-    console.log("referer:", request.get("referer"))
     const result = await supabaseService.insertWebhook(
-        request.get("host"),
+        request.clientIp,
         request.body
     )
     response.json(result)
